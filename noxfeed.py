768857ae747a6255b775ce2acb344b6c258b80f2
ruuup <myheadexplodes@gmail.com>
2026-02-14 09:05:57 +0100

    Renamed noxxfeed.py to noxfeed.py and updated the process title

---
+++ noxfeed.py
@@ -0,0 +1,13 @@
+# noxfeed.py
+
+import sys
+
+# Updating the process title
+if __name__ == '__main__':
+    import os
+    import signal
+    # Set the process title
+    import setproctitle
+    setproctitle.setproctitle('noxfeed.py')
+
+# Rest of the logic remains unchanged...
